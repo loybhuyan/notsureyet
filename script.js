5abe8344f83dd388bc088dd4dde90cfe06db5f54
loybhuyan <abhigyanbhuyan@gmail.com>
2026-02-18 14:56:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('playMusic').addEventListener('click', () => {
+    alert('?? Rock on! Your virtual studio is now live. Every line of code is a note in your masterpiece.');
+});
