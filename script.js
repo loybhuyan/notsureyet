046fa7fdfaf44fe3b40514eef72b84dd6ef6550d
loybhuyan <abhigyanbhuyan@gmail.com>
2026-02-18 15:06:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.getElementById('playMusic').addEventListener('click', () => {
-    alert('?? Rock on! Your virtual studio is now live. Every line of code is a note in your masterpiece.');
+document.getElementById('dropBeat').addEventListener('click', () => {
+    const colors = ['#ff00ff', '#00ffff', '#39ff14', '#ffff00'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    
+    document.querySelector('.psychedelic-bg').style.background = conic-gradient(from 0deg, , #000, );
+    
+    alert('?? Can you feel it? The rhythm is flowing through the code. You are the conductor of this digital symphony!');
 });
